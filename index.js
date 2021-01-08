1da4db04aae5565c99ac0e4b64bde0a44bf963b5
Krishna <krishna05564@gmail.com>
2021-01-08 16:31:57 +0530

    certis code and status update

--- index.js
+++ index.js
@@ -1,13 +1,141 @@
 //intializing aos
 AOS.init();
 
+var jsonData = [
+    {
+        "name":"Web developer bootcamp",
+        "authority":"Udemy",
+        "link":"https://www.udemy.com/certificate/UC-PIOMVMV3/"
+    },
+    {
+        "name":"Introduction to Structured Query Language (SQL)",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/verify/8NGJW6M2FX9H"
+    },
+    {
+        "name":"Programming for Everybody (Getting Started with Python)",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/verify/CPV8PW6WN6NJ"
+    },
+    {
+        "name":"Marketing in a Digital World",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/3U6T7EPA3VBM"
+    },
+    {
+        "name":"Digital Analytics for Marketing Professionals: Marketing Analytics in Theory",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/C8TYYTH8CXCN"
+    },
+    {
+        "name":"Digital Analytics for Marketing Professionals: Marketing Analytics in Practice",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/67967D2H2KHY"
+    },
+    {
+        "name":"Advanced Styling with Responsive Design",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/DRT7WEKDL3LC"
+    },
+    {
+        "name":"IBM Blockchain Foundation for Developers",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/WPJUV27PKSQF"
+    },
+    {
+        "name":"Blockchain Basics",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/GSGT7FAT5J23"
+    },
+    {
+        "name":"Graph Analytics for Big Data",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/RHR4JBLHFHL4"
+    },
+    {
+        "name":"Machine Learning With Big Data",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/KWD7N3JWMFVN"
+    },
+    {
+        "name":"Blockchain A-Z™: Learn How To Build Your First Blockchain",
+        "authority":"Udemy",
+        "link":"https://www.udemy.com/certificate/UC-1SNKT7I7/"
+    },
+    {
+        "name":"Interactivity with JavaScript",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/HAC9KLR6GF26"
+    },
+    {
+        "name":"Big Data Integration and Processing by University of California San Diego",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/7GQH2X4MQKMK"
+    },
+    {
+        "name":"Big Data Modeling and Management Systems by University of California San Diego ",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/VPHBPA326QR3"
+    },
+    {
+        "name":"Introduction to Big Data by University of California San Diego",
+        "authority":"Coursera",
+        "link":"https://www.coursera.org/account/accomplishments/certificate/49URFG2EF88T"
+    }
+
+]
 //sidenav intialization
 document.addEventListener('DOMContentLoaded', function() {
-var elems = document.querySelectorAll('.sidenav');
-var instances = M.Sidenav.init(elems, {});
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+    //adding data to the table 
+    jsonData.forEach((item)=>{
+        createTableDesktop(item)    
+        createTableMobile(item)
+        AOS.refreshHard();
+    })
+    
 });
 
+let createTableDesktop = (item)=>{
+    let table = document.getElementById("certificateTable");
+    let tr = document.createElement("tr")
+    let td0 = document.createElement("td")
+    let td1 = document.createElement("td")
+    let td2 = document.createElement("td")
 
+    td0.innerText = item.name
+    td1.innerText = item.authority
+    tr.appendChild(td0)
+    tr.appendChild(td1)
+
+    let link =  document.createElement("a")
+    link.href = item.link
+    link.innerText = "See Certificate"
+    link.setAttribute("target","_blank")
+    link.classList.add("white-text")
+
+    td2.appendChild(link)
+    tr.appendChild(td2)
+    table.appendChild(tr)
+}
+var globalFadeAOS = 10;
+let createTableMobile = (item) => {
+    let table = document.getElementById("certificateTableMobile");
+    let li = document.createElement("li")
+    let link = document.createElement("a")
+    link.href = item.link
+    link.setAttribute("target","_blank")
+    link.classList.add("grey-text")
+    link.classList.add("text-lighten-3")
+    link.classList.add("truncate")
+    link.setAttribute("data-aos","fade-down")
+    link.innerText ="→ " + item.name + " (" + item.authority + ")"
+    link.setAttribute("data-aos-delay",""+globalFadeAOS)
+    globalFadeAOS = globalFadeAOS + 10
+    li.appendChild(link)
+    table.appendChild(li)
+}
 //certification aniamtion
 
 
